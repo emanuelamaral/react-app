a3e29f193f628735ed6455960dc2794584d84ad2
emanuelamaral <amoz_c.p.t@hotmail.com>
2024-06-01 18:17:54 -0300

    Adequação do codigo antigo ao admin do dashboard

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,8 @@ import Dashboard from "./views/Dashboard.js";
 import Map from "./views/Map.js";
 // import Notifications from "./views/Notifications.js";
 import UserProfile from "./views/UserProfile.js";
+import UserForm from "./views/user/UserForm.js";
+import UserList from "./views/user/UserList.js";
 
 var routes = [
   {
@@ -28,9 +30,23 @@ var routes = [
   {
     path: "/user-profile",
     name: "Perfil de Usuário",
-    icon: "tim-icons icon-single-02",
+    icon: "tim-icons icon-notes",
     component: <UserProfile />,
     layout: "/admin",
   },
+  {
+    path: "/users",
+    name: "Usuários",
+    icon: "tim-icons icon-single-02",
+    component: <UserList />,
+    layout: "/admin",
+  },
+  {
+    path: "/register-user",
+    name: "Cadastro de usuário",
+    icon: "tim-icons icon-badge",
+    component: <UserForm />,
+    layout: "/admin"
+  }
 ];
 export default routes;
