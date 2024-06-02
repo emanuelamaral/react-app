76e9fac095f6d4e85e49bd78b074276eb7962819
Duarte097 <leo_r1089@hotmail.com>
2024-06-02 16:53:07 -0300

    Removendo o MainApp

--- src/App.js
+++ src/App.js
@@ -5,13 +5,9 @@ import AppRoutes from './AppRoutes';
 function App() {
   return (
     <BrowserRouter >
-      <header className='telaInicial'>Minha App</header>
-      <div className="line"></div>
       <main className='telaInicial'>
         <AppRoutes />
       </main>
-      <div className="line"></div>
-      <footer className='telaInicial'>Feito na UTF</footer>
     </BrowserRouter>
   );
 }
